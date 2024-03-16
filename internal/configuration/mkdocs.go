19ea44e59fc7b9937243a1465fc24f7225c53c5e
Markus Ressel <mail@markusressel.de>
2024-03-17 00:40:51 +0100

    updated example config, added blacklist config option

--- internal/configuration/mkdocs.go
+++ internal/configuration/mkdocs.go
@@ -1,7 +1,8 @@
 package configuration
 
 type MkDocsConfiguration struct {
-	ProjectPath string `yaml:"projectPath"`
-	ConfigFile  string `yaml:"configFile"`
-	DocsPath    string `yaml:"docsPath"`
+	ProjectPath string   `yaml:"projectPath"`
+	ConfigFile  string   `yaml:"configFile"`
+	DocsPath    string   `yaml:"docsPath"`
+	Blacklist   []string `yaml:"blacklist"`
 }
